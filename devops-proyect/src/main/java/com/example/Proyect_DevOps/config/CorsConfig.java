1be729ce046e7a5195d4a0df15e8c3dc2c9d546c
Federico Pérez E. <L23051096@saltillo.tecnm.mx>
2026-03-26 10:53:13 -0600

    fix: config

--- devops-proyect/src/main/java/com/example/Proyect_DevOps/config/CorsConfig.java
+++ devops-proyect/src/main/java/com/example/Proyect_DevOps/config/CorsConfig.java
@@ -5,16 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import io.micrometer.common.lang.NonNull;
-
 @Configuration
 public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
             @Override
-            @NonNull
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://dev-server.local:5173", "http://localhost:5173") // frontend React
