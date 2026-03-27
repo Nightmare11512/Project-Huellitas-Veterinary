7cf482813b27d94b0264ca093d81025284264284
Federico Pérez E. <L23051096@saltillo.tecnm.mx>
2026-03-26 19:47:46 -0600

    feat:add security and cripto generation

--- devops-proyect/src/main/java/com/example/Proyect_DevOps/services/UsuarioService.java
+++ devops-proyect/src/main/java/com/example/Proyect_DevOps/services/UsuarioService.java
@@ -1,15 +1,30 @@
 package com.example.Proyect_DevOps.services;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.example.Proyect_DevOps.models.UsuarioModel;
 import com.example.Proyect_DevOps.repositories.UsuarioRepository;
+
+
 
 @Service
 public class UsuarioService {
 
     @Autowired
-    @SuppressWarnings("unused")
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean validacionDeLogin(String correo, String contrasena){
+        Optional<UsuarioModel> usuarioOpt = usuarioRepository.findByCorreo(correo);
+        UsuarioModel usuario = usuarioOpt.get();
+        if (usuarioOpt.isPresent()){
+            return passwordEncoder.matches(contrasena, usuario.getContraseña());
+        } else {
+            return false;
+        }
+    }
 }
