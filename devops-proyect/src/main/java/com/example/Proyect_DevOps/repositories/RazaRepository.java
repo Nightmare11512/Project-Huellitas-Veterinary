d570ff89817a0c848355cab0dbbddb500d521f3f
Federico Pérez E. <L23051096@saltillo.tecnm.mx>
2026-03-29 23:39:06 -0600

    hotfix: reestructure the finder on repository of Raza

--- devops-proyect/src/main/java/com/example/Proyect_DevOps/repositories/RazaRepository.java
+++ devops-proyect/src/main/java/com/example/Proyect_DevOps/repositories/RazaRepository.java
@@ -3,11 +3,13 @@ package com.example.Proyect_DevOps.repositories;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.Proyect_DevOps.models.EspecieModel;
 import com.example.Proyect_DevOps.models.RazaModel;
 
 @Repository
 public interface RazaRepository extends JpaRepository<RazaModel, Integer>{
 
-    List<RazaModel> findByEspecieId (int especieId);
+    List<RazaModel> findByEspecie (EspecieModel especie);
 
 }
