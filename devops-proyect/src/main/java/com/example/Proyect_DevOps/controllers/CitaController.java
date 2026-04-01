00d5cc0d34a585b402fc5fe30ed74baef5b8364a
Federico Pérez E. <L23051096@saltillo.tecnm.mx>
2026-04-01 01:51:36 -0600

    feat: add table to view historic dates

--- devops-proyect/src/main/java/com/example/Proyect_DevOps/controllers/CitaController.java
+++ devops-proyect/src/main/java/com/example/Proyect_DevOps/controllers/CitaController.java
@@ -1,9 +1,36 @@
 package com.example.Proyect_DevOps.controllers;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.Proyect_DevOps.models.UsuarioModel;
+import com.example.Proyect_DevOps.repositories.MascotaRepository;
+import com.example.Proyect_DevOps.repositories.UsuarioRepository;
+import com.example.Proyect_DevOps.models.CitaModel;
+import com.example.Proyect_DevOps.models.MascotaModel;
+import com.example.Proyect_DevOps.services.CitaService;
 
 @RestController
 @RequestMapping("/cita")
 public class CitaController {
+
+    @Autowired
+    private CitaService citaService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private MascotaRepository mascotaRepository;
+
+    @GetMapping("/cita/{correo}/{idMascota}")
+    public List<CitaModel> getListaDeCitasSegunUsuarioYMascota(@PathVariable String correo, @PathVariable int idMascota){
+        UsuarioModel usuario = usuarioRepository.findByCorreo(correo).orElseThrow(()
+         -> new RuntimeException("Usuario no encontrado"));
+        MascotaModel mascota = mascotaRepository.findById(idMascota).orElseThrow(()
+         -> new RuntimeException("Mascota no encontrada"));
+        return citaService.getCitasByUsuarioAndMascota(usuario, mascota);
+    }
 }
--- devops-proyect/src/main/java/com/example/Proyect_DevOps/repositories/CitaRepository.java
+++ devops-proyect/src/main/java/com/example/Proyect_DevOps/repositories/CitaRepository.java
@@ -3,8 +3,14 @@ package com.example.Proyect_DevOps.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.Proyect_DevOps.models.CitaModel;
+import com.example.Proyect_DevOps.models.MascotaModel;
+import com.example.Proyect_DevOps.models.UsuarioModel;
+
+import java.util.List;
+
 
 @Repository
 public interface CitaRepository extends JpaRepository<CitaModel, Integer>{
 
+    List<CitaModel> findByUsuarioMascotaAndMascotaModel (UsuarioModel usuarioMascota, MascotaModel mascotaModel);
 }
